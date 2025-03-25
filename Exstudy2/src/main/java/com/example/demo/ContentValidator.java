69e750a04214257f71ee2cbc035b60322807d4a3
Yim_JunSu <yimjunsu@gmail.com>
2025-03-25 18:09:44 +0900

    ExStudy2, Valid 재 수정 - 유효성 Form 값 검증. ValidationUtils 사용 및 의존성 추가해서  @Valid 어노테이션 사용해보기

--- Exstudy2/src/main/java/com/example/demo/ContentValidator.java
+++ Exstudy2/src/main/java/com/example/demo/ContentValidator.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 public class ContentValidator implements Validator {
@@ -11,15 +12,28 @@ public class ContentValidator implements Validator {
 	@Override
 	public void validate(Object obj, Errors errors) {
 		ContentDto dto = (ContentDto)obj;
-		String nWriter = dto.getWriter();
-		if(nWriter==null || nWriter.trim().isEmpty()) {
-			System.out.println("Writer is null or empty");
-			errors.rejectValue("writer", "trouble");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "writer", "writer is empty");
+		String sWriter = dto.getWriter();
+		// 값 길이 3글자 하면 오류
+		if(sWriter.length() < 3) {
+			errors.rejectValue("writer", "writer is too short.");
 		}
-		String nContent = dto.getContent();
-		if(nContent==null || nContent.trim().isEmpty()) {
-			System.out.println("Content is null or empty");
-			errors.rejectValue("Content", "trouble");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "content", "content is empty");
+		String sContent = dto.getContent();
+		if(sContent.length() < 3) {
+			errors.rejectValue("content", "Content is too short.");
 		}
+//		ch01
+//		String nWriter = dto.getWriter();
+//		만약 비어있거나, 값이 없으면
+//		if(nWriter==null || nWriter.trim().isEmpty()) {
+//			System.out.println("Writer is null or empty");
+//			errors.rejectValue("writer", "trouble");
+//		}
+//		String nContent = dto.getContent();
+//		if(nContent==null || nContent.trim().isEmpty()) {
+//			System.out.println("Content is null or empty");
+//			errors.rejectValue("Content", "trouble");
+//		}
 	}
 }
--- Exstudy2/src/main/java/com/example/demo/ViewController.java
+++ Exstudy2/src/main/java/com/example/demo/ViewController.java
@@ -1,5 +1,53 @@
 package com.example.demo;
 
-public class ViewController {
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+public class ViewController {
+	@RequestMapping("/")
+	public @ResponseBody String root() throws Exception{
+		return "Validator (1)";
+	}
+	@RequestMapping("/insertForm")
+	public String insert1() {
+		return "createPage";
+	}
+	@RequestMapping("/create")
+	// Valid 대신 Validated
+	public String insert2(@ModelAttribute("dto") @Validated ContentDto contentDto, BindingResult result) {
+		String page = "createDonePage";
+		System.out.println(contentDto);
+		
+		//ContentValidator validator = new ContentValidator();
+		//validator.validate(contentDto, result);
+		if(result.hasErrors()) {
+			System.out.println("getAllErrors : " + result.getAllErrors());
+//			if(result.getFieldError("writer") != null) {
+//				System.out.println("1:"+result.getFieldError("writer").getCode());
+//			}
+//			if(result.getFieldError("content") != null) {
+//				System.out.println("2:"+result.getFieldError("content").getCode());
+//			}
+//			page = "createPage";
+			if(result.getFieldError("writer") != null) {
+				System.out.println("1:"+result.getFieldError("writer").getDefaultMessage());
+			}
+			if(result.getFieldError("content") != null) {
+				System.out.println("2:"+result.getFieldError("content").getDefaultMessage());
+			}
+			page = "createPage";
+		}
+		return page;
+	}
+//	@InitBinder
+//	protected void initBinder(WebDataBinder binder) {
+//		binder.setValidator(new ContentValidator());
+//	}
 }
